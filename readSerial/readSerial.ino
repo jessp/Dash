795bf3b87e3a11e5fb74f2804a9a1ca7f518e56c
Jessica <jessicapeter92@gmail.com>
2016-01-04 22:02:22 -0500

    Added updated Arduino file

--- readSerial/readSerial.ino
+++ readSerial/readSerial.ino
@@ -1,6 +1,8 @@
 
 //informed by http://stackoverflow.com/questions/5697047/convert-serial-read-into-a-useable-string-using-arduino
 String receivedString;
+String oneMessage[20];
+String completedMessage = "";
 
 void setup() {
   // Bean Serial is at a fixed baud rate. Changing the value in Serial.begin() has no effect.
@@ -9,16 +11,32 @@ void setup() {
 
 
 void loop() {
-  if(Serial.available() > 0)
-    {
-        receivedString = Serial.readStringUntil('~');
-        Serial.println(receivedString);
-        if (receivedString.equals("Echo")){
-          Serial.println("Ready for duty!");
+  if(Serial.available() > 0) {
+            receivedString = Serial.readStringUntil('~');
+            if (receivedString.length() != 30 && receivedString.charAt(receivedString.length()-1) != '`'){
+              Serial.println("n");
+            } else {
+              Serial.println(receivedString); 
+              completedMessage = completedMessage + receivedString;
+              if (receivedString.charAt(receivedString.length()-1) == '`'){
+              sonifyMessage(completedMessage);
+              completedMessage = "";
+              }
+            }
         }
     }
 
+void sonifyMessage(String oneMessage){
+    for (int i = 0; i < oneMessage.length(); i++){
+      if (int(oneMessage.charAt(i) - '0') == 0){
+          Bean.setLed(0,0,255);  // blue
+      } else if (int(oneMessage.charAt(i) - '0') == 1){
+          Bean.setLed(255,0,0);  // blue
+      } else {
+          Bean.setLed(0,255,0);  // blue
+      }
+    }
+    Bean.setLed(0,0,0);
 }
-
 
 
